65a95b96b7856e79b525fedfbb522feac8c12a1c
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-07-21 22:29:41 +0600

    fetch data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,6 +34,18 @@ async function run() {
         app.get('/services', async(req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('srvices/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const options = {
+                projection: { title: 1, price: 1, service_id: 1 }
+            }
+
+            const result = await serviceCollection.findOne(query, options);
             res.send(result);
         })
 
