458de1fa600a8782d5344d5fd37b2a2ffa41cb2b
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-09-06 00:47:42 +0600

    some changes

--- index.js
+++ index.js
@@ -51,6 +51,12 @@ async function run() {
         })
 
         // Bookings
+        app.get('/bookings', async(req, res) => {
+            const result = await bookingCollection.find({}).toArray();
+            res.send(result);
+        })
+
+
         app.post('/bookings', async(req, res) => {
             const booking = req.body;
             console.log(booking);
